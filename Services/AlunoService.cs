673633e7183b4fbaa5074bc47b79c029a85cf3ce
Arthur Jorge <arthurjorge.contato@gmail.com>
2026-03-29 17:44:46 -0300

    Implementando Aluno Service com CRUD

---
+++ Services/AlunoService.cs
@@ -0,0 +1,73 @@
+using System.Data.Common;
+using CadastroAlunos.Models;
+public class AlunoService
+{
+    private List<Aluno> alunos = new List<Aluno>();
+    private int proximoId = 1;
+
+    public List<Aluno> Alunos
+    {
+        get{return this.Alunos;}
+        set{this.Alunos = value;}
+    }
+    public int ProximoId
+    {
+        get{return this.proximoId;}
+        set{this.proximoId = value;}
+    }
+    public AlunoService(List<Aluno> alunos, int proximoId)
+    {
+        this.alunos = alunos;
+        this.proximoId = proximoId;
+    }
+    public void Cadastrar(Aluno aluno)
+    {
+        aluno.Id = proximoId;
+        alunos.Add(aluno);
+        proximoId++;
+    }
+    public void ListarTodos()
+    {
+        if (alunos.Count == 0)
+        {
+            Console.WriteLine("Nenhum aluno cadastrado");
+        }
+        foreach (Aluno i in alunos)
+        {
+            Console.WriteLine(i.ExibirInfo);
+        }    
+    }
+    public void BuscarPorNome(string nome)
+    {
+        bool encontrou = false;
+        foreach (Aluno i in alunos)
+        {
+            if (i.Nome.Contains(nome))
+            {
+                encontrou = true;
+                i.ExibirInfo();
+            }
+        }
+        if (encontrou == false)
+        {
+            Console.WriteLine("Nenhum aluno encontrado.");
+        }  
+    }
+    public void Remover(int id)
+    {
+        bool encontrou = false;
+        foreach (Aluno i in alunos)
+        {
+            if (i.Id == id)
+            {
+                alunos.Remove(i);
+                encontrou = true;
+                break;
+            }
+        }
+        if (encontrou == false)
+        {
+            Console.WriteLine("Aluno não encontrado.");
+        }
+    }
+}
